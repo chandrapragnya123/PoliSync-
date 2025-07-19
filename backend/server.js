c763b3002cd0ed44f6e9a9a9e84293666d3007c7
PRAGNYA CHANDRA <22wh1a1290@bvrithyderabad.edu.in>
2025-07-20 00:33:24 +0530

    Update CORS for Netlify frontend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/uploads', express.static('uploads'));
 // ------------------------------------
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://polisyncwebsite2.netlify.app',
   credentials: true
 }));
 
